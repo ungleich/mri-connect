2428818cf6d060e2335bff391de73eb5b6716175
Oleg Lavrovsky <oleg@utou.ch>
2018-08-23 07:18:38 +0200

    Configuration view

--- app/views.py
+++ app/views.py
@@ -4,19 +4,36 @@
 from app import app, db, admin
 from .models import *
 from flask_admin.contrib.sqla import ModelView, filters
+from flask_admin.form import FileUploadField
+from flask_admin import (
+    BaseView, expose
+)
 from flask import (
     url_for, redirect,
     request, flash,
     render_template,
-    send_from_directory
+    send_from_directory,
 )
+from werkzeug import secure_filename
 
-from os.path import isfile
 import csv
+import os.path as ospath
+from shutil import move
+from tempfile import gettempdir
+
+# Get temporary file storage
+UPLOAD_PATH = gettempdir()
+DATAFILE = ospath.join(ospath.dirname(__file__), '..', 'data', 'people_details.csv')
 
 # Add views
-admin.add_view(ModelView(Person, db.session))
-admin.add_view(ModelView(Resource, db.session))
+PersonView = ModelView(Person, db.session)
+PersonView.column_list = ['first_name', 'last_name', 'organisation']
+admin.add_view(PersonView)
+
+ResourceView = ModelView(Resource, db.session)
+ResourceView.column_list = ['title', 'citation', 'url']
+admin.add_view(ResourceView)
+
 
 # API views
 @app.route("/api/people", methods=['GET'])
@@ -35,6 +52,14 @@ def people_detail(people_id):
 def resources_list():
     return [r.dict() for r in Resource.query.limit(10).all()]
 
+# Custom view
+class ConfigurationView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/config.html')
+
+admin.add_view(ConfigurationView(name='Configuration', endpoint='config'))
+
 # Static paths
 @app.route('/data/<path:path>')
 def send_data(path):
@@ -46,20 +71,51 @@ def send_client(path):
 def send_static(path):
     return send_from_directory('../static', path)
 
+# Data upload
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_data():
+    if request.method == 'POST' and 'datafile' in request.files:
+        fs = request.files['datafile']
+        fs_name = secure_filename(fs.filename)
+        fs_path = ospath.join(UPLOAD_PATH, fs_name)
+        fs.save(fs_path)
+        # Validation
+        count = 0
+        with open(fs_path, 'rt') as csvfile:
+            datareader = csv.DictReader(csvfile)
+            for row in datareader:
+                if row is None: continue
+                person = Person(first_name=row['First name'], last_name=row['Last name'])
+                person.title = row['Title']
+                person.organisation = row['Organisation English']
+                person.country = row['Country']
+                person.biography = row['Biography']
+                person.contact_email = row['e-mail 1']
+                person.personal_url = row['URL']
+                count = count + 1
+        if count > 0:
+            move(fs_path, DATAFILE)
+        flash("Uploaded and validated %d rows" % count)
+        return refresh_data()
+    else:
+        flash("Please select a valid file")
+    return redirect(url_for('config.index'))
+
 # Data update
 @app.route('/refresh', methods=["POST"])
 def refresh_data():
-    if not isfile('data/people_details.csv'):
+    if not ospath.isfile(DATAFILE):
         flash("Add people_details, resources CSV files to the data folder first.")
         return redirect(url_for('admin.index'))
 
     count_p = 0
-    with open('data/people_details.csv', 'rt') as csvfile:
+    with open(DATAFILE, 'rt') as csvfile:
         datareader = csv.DictReader(csvfile)
         for row in datareader:
             if row is None: continue
             person = Person.query.filter_by(first_name=row['First name'], last_name=row['Last name']).first()
-            if not person:  person = Person(first_name=row['First name'], last_name=row['Last name'])
+            if not person:
+                person = Person(first_name=row['First name'], last_name=row['Last name'])
             person.title = row['Title']
             person.organisation = row['Organisation English']
             person.country = row['Country']
@@ -87,5 +143,5 @@ def refresh_data():
             count_r = count_r + 1
         db.session.commit()
 
-    flash("%d people and %d resources updated" % (count_p, count_r))
-    return redirect(url_for('admin.index'))
+    flash("%d people and %d resources updated from dataset" % (count_p, count_r))
+    return redirect(url_for('config.index'))
